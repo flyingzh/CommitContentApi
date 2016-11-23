41d76d89f6635d224c98ed1dacb5b558707b14d5
yuanyang <yuan823978@gmail.com>
2016-11-23 13:13:04 +0800

    first commit

---
+++ app/src/main/java/com/example/android/commitcontent/ime/MyAty.java
@@ -0,0 +1,15 @@
+package com.example.android.commitcontent.ime;
+
+import android.app.Activity;
+
+/**
+ * Created by yuanyang on 16/11/23.
+ */
+
+public class MyAty extends Activity {
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(R.layout.activity_my);
+    }
+}
